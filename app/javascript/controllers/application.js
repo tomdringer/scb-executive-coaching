639c4327686ac5fdff6ecd6042cf02edadbaa0d7
Tom Dringer <47415157+tomdringer@users.noreply.github.com>
2024-06-29 21:01:36 +0100

    Rebundled js

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,11 +1,9 @@
 import { Application } from "@hotwired/stimulus"
 import ScrollTo from "@stimulus-components/scroll-to";
-import Carousel from "stimulus-carousel"
 
 window.Stimulus = Application.start()
 
 Stimulus.register("scrollTo", ScrollTo);
-Stimulus.register("carousel", Carousel)
 
 
 // Configure Stimulus development experience
