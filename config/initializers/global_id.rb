034974e014300b5676883efb8b564418d2d12f29
Tom Dringer <47415157+tomdringer@users.noreply.github.com>
2024-07-17 20:37:36 +0100

    New global_id initializer

--- config/initializers/global_id.rb
+++ config/initializers/global_id.rb
@@ -1,5 +1,14 @@
-Rails.application.config.after_initialize do
-  if Rails.application.secrets.secret_key_base
-    SignedGlobalID.verifier = ActiveSupport::MessageVerifier.new(Rails.application.secrets.secret_key_base)
-  end
+# config/initializers/global_id.rb
+require 'active_support/message_verifier'
+
+Rails.application.config.to_prepare do
+  key_generator = ActiveSupport::KeyGenerator.new(
+    Rails.application.secret_key_base,
+    iterations: 1000
+  )
+
+  secret = key_generator.generate_key('signed global id')
+  verifier = ActiveSupport::MessageVerifier.new(secret)
+
+  GlobalID::Locator.verifier = verifier
 end
