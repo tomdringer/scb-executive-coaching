7e1f3fbb5825419f769bcbbc3687909f721355f6
Tom Dringer <47415157+tomdringer@users.noreply.github.com>
2024-07-17 20:32:49 +0100

    Added global to about model

--- app/models/about.rb
+++ app/models/about.rb
@@ -2,4 +2,8 @@ class About < ApplicationRecord
   has_rich_text :body
   has_one_attached :photo_one
   has_one_attached :photo_two
+
+  about = Intro.last
+  about.to_global_id.to_s
+  about.to_signed_global_id.to_s
 end
