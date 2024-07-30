e4f8f8367246018ba924476d324eae0586c55cbb
Tom Dringer <47415157+tomdringer@users.noreply.github.com>
2024-07-30 19:47:13 +0100

    New image storage option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # Store uploaded files on the local file system (see config/storage.yml for options).
+  config.active_storage.service = :render
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
