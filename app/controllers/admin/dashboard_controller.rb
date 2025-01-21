8807be06e29d2d4387b79c21544257ce22ac0367
Tom Dringer <47415157+tomdringer@users.noreply.github.com>
2025-01-21 19:09:58 +0000

    Deleted redundant controllers/dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,20 +1,20 @@
 module Admin
-class DashboardController < ApplicationController
-  before_action :authenticate_user!
-  
-  def index
-    @menu_sections = Section.uncached do
-      Section.where(hide_menu: false).order(Arel.sql('COALESCE(position, 999999), id'))
+  class DashboardController < ApplicationController
+    before_action :authenticate_user!
+    
+    def index
+      @menu_sections = Section.uncached do
+        Section.where(hide_menu: false).order(Arel.sql('COALESCE(position, 999999), id'))
+      end
+      Rails.logger.debug "Menu Sections: #{@menu_sections.map { |s| { id: s.id, position: s.position } }}"
+      @blogs = Blog.all
+      @resources = Resource.all
+      @sections = Section.all
+      @users = User.all
+      @reviews = Review.all
+      @categories = Category.all
+      @policies = Policy.all
+      @coaching = Coaching.all
     end
-    Rails.logger.debug "Menu Sections: #{@menu_sections.map { |s| { id: s.id, position: s.position } }}"
-    @blogs = Blog.all
-    @resources = Resource.all
-    @sections = Section.all
-    @users = User.all
-    @reviews = Review.all
-    @categories = Category.all
-    @policies = Policy.all
-    @coaching = Coaching.all
   end
-end
 end
